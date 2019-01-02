e2e28d42cdf3e24c2eb663c5d81fef73f1d9f352
anrong_wn <anrong_wn@126.com>
2019-01-02 15:45:48 +0800

    /**  @loop: =nullptr, 自创loop及uv_run线程    ！=nullptr, 使用loop 及 loop所在的uv_run线程  @cb: 处理器  */

--- anMessageEngine/anMee.h
+++ anMessageEngine/anMee.h
@@ -38,7 +38,12 @@ private:
 public:
 	anMee();
 	~anMee();
-
+	
+	/**
+	@loop:	=nullptr, 自创loop及uv_run线程
+			！=nullptr, 使用loop 及 loop所在的uv_run线程
+	@cb:	处理器
+	*/
 	void start(uv_loop_t* loop, handle_fn cb);
 	void stop();
 	void push(const char*data, size_t len);
